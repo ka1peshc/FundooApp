fc26a326d785d2eda431c0f294c66955c95f0914
ka1peshc <kalpesh111997@gmail.com>
2021-11-16 15:25:33 +0530

    [Kalpesh]Add: RegisterModel to get new user data

---
+++ FundooModels/RegisterModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace FundooModels
+{
+    public class RegisterModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+
+    }
+}
