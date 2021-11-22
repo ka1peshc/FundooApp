e438e4c13a0b1d360647cb76339aec74917cd997
ka1peshc <kalpesh111997@gmail.com>
2021-11-22 17:36:36 +0530

    [Kalpesh]Add Migration for Notes

---
+++ FundooRepository/Migrations/20211122115044_notes.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FundooRepository.Migrations
+{
+    public partial class notes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notes",
+                columns: table => new
+                {
+                    NoteId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(nullable: true),
+                    NoteBody = table.Column<string>(nullable: true),
+                    PinBody = table.Column<bool>(nullable: false),
+                    RemindMe = table.Column<bool>(nullable: false),
+                    Color = table.Column<string>(nullable: true),
+                    AddImage = table.Column<string>(nullable: true),
+                    IsArchive = table.Column<bool>(nullable: false),
+                    IsTrash = table.Column<bool>(nullable: false),
+                    UserID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notes", x => x.NoteId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notes");
+        }
+    }
+}
