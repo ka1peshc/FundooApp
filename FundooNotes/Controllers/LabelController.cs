4f969f8baa64a92e69d67b847e2fb2cd5793a668
ka1peshc <kalpesh111997@gmail.com>
2021-11-26 16:33:06 +0530

    [Kalpesh]Add AddLabelToAccount function in controller

--- FundooNotes/Controllers/LabelController.cs
+++ FundooNotes/Controllers/LabelController.cs
@@ -34,7 +34,30 @@ namespace FundooNotes.Controllers
                 }
 
             }
-            catch(ArgumentNullException ex)
+            catch (ArgumentNullException ex)
+            {
+                return this.NotFound(new ResponseModel<string>() { Status = false, Message = ex.Message });
+            }
+        }
+
+        [HttpPost]
+        [Route("api/createNoteforAccount")]
+        public async Task<IActionResult> AddLabelToAccount(LabelModel labelData)
+        {
+            try
+            {
+                string result = await this.labelManager.AddLabelToAccount(labelData);
+                if (result == "Label added to note")
+                {
+                    return this.Ok(new ResponseModel<string>() { Status = true, Message = result, Data = " Session data" });
+                }
+                else
+                {
+                    return this.BadRequest(new ResponseModel<string>() { Status = false, Message = result });
+                }
+
+            }
+            catch (ArgumentNullException ex)
             {
                 return this.NotFound(new ResponseModel<string>() { Status = false, Message = ex.Message });
             }
